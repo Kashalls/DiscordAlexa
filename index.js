c6b72f6a67cd605881949077ce59989a24e9e064
Kashall <jordpjones@gmail.com>
2018-05-15 19:08:57 -0700

    Add some more :)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 if (process.version.slice(1).split(".")[0] < 8) throw new Error("Node 8.0.0 or higher is required. Update Node on your system.");
 
-const result = dotenv.config();
+const result = require('dotenv').config()
 
 if (result.error) {
   throw result.error;
